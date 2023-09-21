fa60d2012f7cb2349dc71dd0efe1d1c63ee9ff3f
AlinaCalmis <alinacalmis1@gmail.com>
2023-09-21 21:49:03 +0300

    added subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit , OnDestroy{
+  activatedUser = false;
+  private subscription : Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.userService.activatetdSubject.subscribe((value)=> {
+      this.activatedUser = value
+    })
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
